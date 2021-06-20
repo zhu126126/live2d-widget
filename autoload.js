6ea357ae5778b349b88387d6287bf02ab4eec66e
乘船听雨 <zhu126229736@126.com>
2021-06-20 11:46:20 +0800

    v1.0.2

--- autoload.js
+++ autoload.js
@@ -1,4 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
+// const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 const live2d_path = "https://cdn.jsdelivr.net/gh/zhu126126/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
